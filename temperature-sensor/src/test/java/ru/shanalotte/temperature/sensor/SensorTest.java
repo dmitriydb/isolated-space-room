5c63cb89969c8745b7340398b2835b7d298734d4
Dmitriy Drizhilo <drizhiloda@gmail.com>
2022-08-17 11:18:23 +0500

    Add new stage in docker builds

--- temperature-sensor/src/test/java/ru/shanalotte/temperature/sensor/SensorTest.java
+++ temperature-sensor/src/test/java/ru/shanalotte/temperature/sensor/SensorTest.java
@@ -1,24 +1,8 @@
 package ru.shanalotte.temperature.sensor;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import static org.mockito.Matchers.any;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @Slf4j
 public class SensorTest {
@@ -35,104 +19,6 @@ public class SensorTest {
     Sensor sensor = new Sensor("test");
     assertThat(sensor.getServerHost()).isEqualTo("localhost");
     assertThat(sensor.getServerPort()).isEqualTo(8172);
-  }
-
-
-  @Test
-  public void should_read_fromSocketServer() throws InterruptedException {
-    Sensor sensor = new Sensor("test");
-    new Thread(new FakeServer("Hello")).start();
-    sensor.startSensing();
-    Thread.sleep(150);
-    assertThat(sensor.getRecordedEvents()).containsAll(Collections.singleton("Hello"));
-  }
-
-  @Test
-  public void should_sendRecordedEventsToTopic() throws InterruptedException {
-    new Thread(new FakeServer("a", "b", "c")).start();
-    Sensor sensor = new Sensor("test");
-    SensorProducer sensorProducer = Mockito.mock(SensorProducer.class);
-    sensor.attachProducer(sensorProducer);
-    sensor.startSensing();
-    ArgumentCaptor<String> recordSendCaptor = ArgumentCaptor.forClass(String.class);
-    Thread.sleep(150);
-    verify(sensorProducer, times(3)).sendRecord(recordSendCaptor.capture());
-    List<String> actuallySendRecords = recordSendCaptor.getAllValues();
-    assertThat(actuallySendRecords).contains("a", "c", "b");
-  }
-
-  @Test
-  public void should_notSendDuplicateEvents() throws InterruptedException {
-    new Thread(new FakeServer("a", "a", "b")).start();
-    Sensor sensor = new Sensor("test");
-    SensorProducer sensorProducer = Mockito.mock(SensorProducer.class);
-    sensor.attachProducer(sensorProducer);
-    sensor.startSensing();
-    Thread.sleep(150);
-    verify(sensorProducer, times(2)).sendRecord(any());
-  }
-
-  @Test
-  public void should_sendDuplicateEventsThatAreApartFromEachOther() throws InterruptedException {
-    new Thread(new FakeServer("a", "b", "a", "b")).start();
-    Sensor sensor = new Sensor("test");
-    SensorProducer sensorProducer = Mockito.mock(SensorProducer.class);
-    sensor.attachProducer(sensorProducer);
-    sensor.startSensing();
-    Thread.sleep(100);
-    verify(sensorProducer, times(4)).sendRecord(any());
-  }
-
-  @Test
-  public void should_readAgainAfterServerIsShuttedDown() throws InterruptedException {
-    new Thread(new FakeServer("a", "b", "c")).start();
-    CountDownLatch countDownLatch = new CountDownLatch(1);
-    new Thread(new FakeServer(countDownLatch, "d", "e", "f")).start();
-    Sensor sensor = new Sensor("test");
-    SensorProducer sensorProducer = Mockito.mock(SensorProducer.class);
-    sensor.attachProducer(sensorProducer);
-    sensor.startSensing();
-    Thread.sleep(200);
-    countDownLatch.countDown();
-    Thread.sleep(200);
-    assertThat(sensor.getRecordedEvents()).contains("a", "c", "b", "d", "e", "f");
-  }
-
-  private static class FakeServer implements Runnable{
-    private final String[] recordsToSend;
-    private CountDownLatch countDownLatch;
-
-    public FakeServer(String... recordsToSend) {
-      this.recordsToSend = recordsToSend;
-    }
-
-    public FakeServer(CountDownLatch countDownLatch, String... recordsToSend) {
-      this.recordsToSend = recordsToSend;
-      this.countDownLatch = countDownLatch;
-    }
-
-
-    @Override
-    public void run() {
-      try {
-        if (countDownLatch != null) {
-          countDownLatch.await();
-        }
-        log.info("Starting server...");
-        @Cleanup ServerSocket serverSocket = new ServerSocket(8172);
-        log.info("Started server");
-        @Cleanup Socket s = serverSocket.accept();
-        log.info("Got connection");
-        @Cleanup PrintWriter out = new PrintWriter(s.getOutputStream());
-        for (String record : recordsToSend) {
-          out.write(record + "\n");
-        }
-        out.flush();
-        System.out.println("Server is shutting down.");
-      } catch (Exception ex) {
-        log.error("", ex);
-      }
-    }
   }
 
 }
--- temperature-service/src/test/java/ru/shanalotte/temperature/socket/TemperatureSocketServerTest.java
+++ temperature-service/src/test/java/ru/shanalotte/temperature/socket/TemperatureSocketServerTest.java
@@ -1,130 +1,14 @@
 package ru.shanalotte.temperature.socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Cleanup;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import ru.shanalotte.config.TemperatureSocketServerConfig;
-import ru.shanalotte.temperature.generator.CollectionTemperatureStateListener;
 import ru.shanalotte.temperature.generator.SimpleTemperatureGenerator;
-import ru.shanalotte.temperature.generator.TemperatureGenerator;
-import ru.shanalotte.schemas.TemperatureState;
-import ru.shanalotte.temperature.generator.TemperatureStateListener;
 
 class TemperatureSocketServerTest {
 
   private ObjectMapper objectMapper = new ObjectMapper();
-
-  @Test
-  public void socket_ShouldGiveCurrentTemperature() throws IOException, InterruptedException {
-    TemperatureGenerator temperatureGenerator = new SimpleTemperatureGenerator();
-    TemperatureStateListener listener = new CollectionTemperatureStateListener();
-    TemperatureSocketServer temperatureSocketServer = new TemperatureSocketServer(9999, temperatureGenerator);
-    temperatureGenerator.addListener(temperatureSocketServer);
-    temperatureGenerator.addListener(listener);
-    temperatureGenerator.start();
-    temperatureSocketServer.start();
-    @Cleanup Socket s = new Socket("localhost", 9999);
-    @Cleanup BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-    int maxEventsToTest = 0;
-    List<String> recordedSocketEvents = new ArrayList<>();
-    String line = null;
-    while ((line = in.readLine()) != null) {
-      maxEventsToTest++;
-      recordedSocketEvents.add(line);
-      if (maxEventsToTest >= 10) {
-        break;
-      }
-    }
-    List<TemperatureState> listenerStates = new ArrayList<>(listener.recordedEvents());
-    List<String> actualEvents = new ArrayList<>();
-    for (TemperatureState e : listenerStates) {
-      String writeValueAsString = objectMapper.writeValueAsString(e.toRecord());
-      actualEvents.add(writeValueAsString);
-    }
-    assertThat(actualEvents).containsAll(recordedSocketEvents.subList(0, recordedSocketEvents.size() - 1));
-  }
-
-
-  @Test
-  public void multipleSocketsSupport() throws IOException, InterruptedException {
-    TemperatureGenerator temperatureGenerator = new SimpleTemperatureGenerator();
-    TemperatureStateListener listener = new CollectionTemperatureStateListener();
-    TemperatureSocketServer temperatureSocketServer = new TemperatureSocketServer(9998, temperatureGenerator);
-    temperatureGenerator.addListener(temperatureSocketServer);
-    temperatureGenerator.addListener(listener);
-    temperatureGenerator.start();
-    temperatureSocketServer.start();
-    Runnable r1 = () -> {
-      try {
-        @Cleanup Socket s = new Socket("localhost", 9998);
-        @Cleanup BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        List<String> recordedEvents = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-          recordedEvents.add(in.readLine());
-        }
-        List<TemperatureState> listenerStates = new ArrayList<>(listener.recordedEvents());
-        List<String> actualEvents = new ArrayList<>();
-        for (TemperatureState e : listenerStates) {
-          String writeValueAsString = objectMapper.writeValueAsString(e.toRecord());
-          actualEvents.add(writeValueAsString);
-        }
-        assertThat(actualEvents).containsAll(recordedEvents.subList(0, recordedEvents.size() - 1));
-
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    };
-    Runnable r2 = () -> {
-      try {
-        @Cleanup Socket s = new Socket("localhost", 9998);
-        @Cleanup BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        List<String> recordedEvents = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-          recordedEvents.add(in.readLine());
-        }
-        List<TemperatureState> listenerStates = new ArrayList<>(listener.recordedEvents());
-        List<String> actualEvents = new ArrayList<>();
-        for (TemperatureState e : listenerStates) {
-          String writeValueAsString = objectMapper.writeValueAsString(e.toRecord());
-          actualEvents.add(writeValueAsString);
-        }
-        assertThat(actualEvents).containsAll(recordedEvents.subList(0, recordedEvents.size() - 1));
-
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    };
-    Runnable r3 = () -> {
-      try {
-        @Cleanup Socket s = new Socket("localhost", 9998);
-        @Cleanup BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        List<String> recordedEvents = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-          recordedEvents.add(in.readLine());
-        }
-        List<TemperatureState> listenerStates = new ArrayList<>(listener.recordedEvents());
-        List<String> actualEvents = new ArrayList<>();
-        for (TemperatureState e : listenerStates) {
-          String writeValueAsString = objectMapper.writeValueAsString(e.toRecord());
-          actualEvents.add(writeValueAsString);
-        }
-        assertThat(actualEvents).containsAll(recordedEvents.subList(0, recordedEvents.size() - 1));
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    };
-    new Thread(r1).start();
-    new Thread(r2).start();
-    new Thread(r3).start();
-    Thread.sleep(2000L);
-  }
 
   @Test
   public void shouldRunOnDefaultPortIfTSocketVariableMissing() {
@@ -132,7 +16,7 @@ class TemperatureSocketServerTest {
       return;
     }
     TemperatureSocketServer socketServer = new TemperatureSocketServer(new SimpleTemperatureGenerator());
-    assertThat(String.valueOf(socketServer.getPort()))
+    assertThat(socketServer.getPort())
         .isEqualTo(TemperatureSocketServerConfig.DEFAULT_PORT);
   }
 
